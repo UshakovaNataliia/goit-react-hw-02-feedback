e29ca0341dbc3f7d15cfd29bec0bb5fa47de5fad
UshakovaNataliia <65922563+UshakovaNataliia@users.noreply.github.com>
2020-11-24 19:17:33 +0200

    Update Statistics.js

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -10,7 +10,7 @@ const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
             <p>Neutral: {neutral}</p>
             <p>Bad: {bad}</p>
             <p>Total: {total}</p>
-            <p>Positive feedback: {positivePercentage} %</p>
+            <p>Positive feedback: {positivePercentage}%</p>
         </>
     )
 };
